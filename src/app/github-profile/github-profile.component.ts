bcc52f0bd62969b186fed76fa2571651bd606150
hym <yaquehym@hotmail.com>
2024-06-07 16:43:11 -0600

    132. Why Route Parameters Are Observables

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -11,7 +11,12 @@ export class GithubProfileComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    //this will be called on when the component is render
+    console.log("GitHubProfile on Init")
+    //use this form to do not use observables
+    let id = this.route.snapshot.paramMap.get('id');
     this.route.paramMap
+      // this will called even if I stay in the same page
       .subscribe( params => {
         let id = +params.get('id');
         console.log(id);
