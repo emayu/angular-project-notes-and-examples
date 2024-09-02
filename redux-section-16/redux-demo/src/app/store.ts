7d200a28bc4b4e3cc94f0b640a68feb2f6e18af8
emayu <yaquehym@hotmail.com>
2024-09-02 14:58:32 -0600

    238. Working with Actions

--- redux-section-16/redux-demo/src/app/store.ts
+++ redux-section-16/redux-demo/src/app/store.ts
@@ -1,7 +1,18 @@
-export interface IAppState {
+import { INCREMENT } from "./actions";
 
+export interface IAppState {
+    counter: number;
 }
 
-export function rootReducer(state, action){
+export const INITAL_STATE: IAppState = {
+    counter: 0
+}
+
+export function rootReducer(state:IAppState, action):IAppState{
+
+    switch(action.type){
+        case INCREMENT: return { counter: state.counter + 1 }
+    }
+
     return state;
 }
