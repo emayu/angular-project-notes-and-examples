88639118826465f03ea83ae174b804067e65caa5
emayu <yaquehym@hotmail.com>
2024-09-03 16:35:08 -0600

    245. Refactoring Fat Case Statement

--- redux-section-16/redux-exercise/src/app/store.ts
+++ redux-section-16/redux-exercise/src/app/store.ts
@@ -20,36 +20,48 @@ export const INITIAL_STATE:IAppState = {
 }
 
 
+const addTodo = (state: IAppState, action): IAppState => {
+    const now = new Date();
+    const newTodo = {
+        id: now.getTime(),
+        title: action.title,
+        isCompleted: false
+    };
+    return tassign(state,
+        {
+            todos: state.todos.concat(newTodo),
+            lastUpdate: now
+        });
+}
+
+const togleTodo = (state: IAppState, action): IAppState => {
+    const targetTodo = state.todos.find(t => t.id === action.id);
+    const targetIndex = state.todos.indexOf(targetTodo);
+    const beforeE = state.todos.slice(0, targetIndex);
+    const afterE = state.todos.slice(targetIndex + 1);
+    return tassign(state,
+        {
+            todos: [...beforeE,
+            tassign(targetTodo, { isCompleted: !targetTodo.isCompleted }),
+            ...afterE],
+            lastUpdate: new Date()
+        });
+}
+
+const removeTodo = (state: IAppState, action): IAppState => {
+    const newTodos = state.todos.filter(t => t.id !== action.id);
+    return tassign(state, { todos: newTodos, lastUpdate: new Date() });
+}
+
+
 export function rootReducer(state:IAppState, action):IAppState{
     switch (action.type) {
-        case ADD_TODO:
-            const now = new Date();
-            const newTodo = {
-                id: now.getTime(),
-                title: action.title,
-                isCompleted: false
-            };
-            return tassign(state,
-                {
-                    todos: state.todos.concat(newTodo),
-                    lastUpdate: now
-                });
+        case ADD_TODO: 
+            return addTodo(state, action);
         case REMOVE_TODO:
-            const newTodos = state.todos.filter(t => t.id !== action.id);
-            return tassign(state, { todos: newTodos, lastUpdate: new Date() });
+            return removeTodo(state, action);
         case TOGLE_TODO:
-            const targetTodo = state.todos.find(t => t.id === action.id);
-            const targetIndex = state.todos.indexOf(targetTodo);
-            const beforeE = state.todos.slice(0, targetIndex);
-            const afterE = state.todos.slice(targetIndex + 1);
-            return tassign(state,
-                {
-                    todos: [...beforeE,
-                    tassign(targetTodo, { isCompleted: !targetTodo.isCompleted }),
-                    ...afterE],
-                    lastUpdate: new Date()
-                });
-
+            return togleTodo(state, action)
         case CLEAR_TODOS:
             return tassign(state, { todos: [], lastUpdate: new Date() })
     }
