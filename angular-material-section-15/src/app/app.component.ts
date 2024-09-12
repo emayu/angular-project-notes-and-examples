61e79ec59924bcdad9af3a7398f4aacf16e984b2
emayu <yaquehym@hotmail.com>
2024-09-12 11:49:35 -0600

    211. Check Boxes

--- angular-material-section-15/src/app/app.component.ts
+++ angular-material-section-15/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-demo';
+
+  isChecked = true;
+
+  onChange($event){
+    console.log($event);
+  }
 }
