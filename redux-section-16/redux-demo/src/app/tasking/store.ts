bea1e2e35ff1119efb778879464569bed8c13dc4
emayu <yaquehym@hotmail.com>
2024-09-12 08:50:12 -0600

    244. Calling Backend APIs

--- redux-section-16/redux-demo/src/app/tasking/store.ts
+++ redux-section-16/redux-demo/src/app/tasking/store.ts
@@ -1,35 +1,49 @@
 import { tassign } from 'tassign'; 
-import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO, CLEAR_TODOS, } from './actions'; 
+import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO, CLEAR_TODOS, FETCH_TODOS_REQUEST, FETCH_TODOS_SUCESS, FETCH_TODOS_ERROR, PROCESSING_REQUEST, } from './actions'; 
 
 export interface ITaskingState {
-  todos: any[];
-  lastUpdate: Date; 
+  todos: ITodo[];
+  lastUpdate: Date;
+  loadingList: boolean;
+  processing: boolean;
+  error: Error; 
+}
+
+export interface  ITodo {
+  userId:number;
+  id: number;
+  title: String;
+  completed: boolean;
 }
 
 export const TASKING_INITIAL_STATE: ITaskingState = { 
   todos: [],
   lastUpdate: null,
+  loadingList: false,
+  processing: false,
+  error: null,
 }
 
 function addTodo(state, action) {
-  var newTodo = { id: state.todos.length + 1, title: action.title };
+  //var newTodo = { id: state.todos.length + 1, title: action.title };
 
   return tassign(state, {
-    todos: state.todos.concat(newTodo),
-    lastUpdate: new Date()
+    todos: [action.todo].concat(state.todos),
+    lastUpdate: new Date(),
+    processing: false
   });
 }
 
-function toggleTodo(state, action) {
-  var todo = state.todos.find(t => t.id === action.id);
-
+function toggleTodo(state:ITaskingState, action) {
+  const todo:ITodo = state.todos.find(t => t.id === action.id);
+  console.log('found', todo)
   // Now, we need to find the position of this item in the array. 
-  var index = state.todos.indexOf(todo);
-
+  const index = state.todos.indexOf(todo);
+  console.log('index', index);
   return tassign(state, {
     todos: [
       ...state.todos.slice(0, index),
-      tassign(todo, { isCompleted: !todo.isCompleted }),
+      tassign(todo, { completed: !todo.completed }),
       ...state.todos.slice(index + 1),
     ],
     lastUpdate: new Date()
@@ -50,6 +64,25 @@ function clearTodos(state, action) {
   });
 }
 
+const fetchTodos = (state:ITaskingState, action) => {
+  return tassign(state, {loadingList: true});
+}
+
+
+const fetchTodosSucess = (state:ITaskingState, action ) => {
+  console.log('changing to false');
+  return tassign(state, {todos: [...action.todos], loadingList: false})
+}
+
+const fetchTodosError = (state:ITaskingState, action ) => {
+  return tassign(state, {loadingList: false, error: tassign(action.error, {})});
+}
+
+
+const processingRequest = (state:ITaskingState, action ) => {
+  return tassign(state, {processing:true});
+}
+
 
 export function taskingReducer(state: ITaskingState = TASKING_INITIAL_STATE, action): ITaskingState {
   switch (action.type) {
@@ -57,6 +90,10 @@ export function taskingReducer(state: ITaskingState = TASKING_INITIAL_STATE, act
     case TOGGLE_TODO: return toggleTodo(state, action);
     case REMOVE_TODO: return removeTodo(state, action);
     case CLEAR_TODOS: return clearTodos(state, action);
+    case FETCH_TODOS_REQUEST: return fetchTodos(state, action);
+    case FETCH_TODOS_SUCESS: return fetchTodosSucess(state, action);
+    case FETCH_TODOS_ERROR: return fetchTodosError(state, action);
+    case PROCESSING_REQUEST: return processingRequest(state, action);
   }
 
   return state; 
--- redux-section-16/redux-demo/src/app/tasking/todo-list/todo-list.component.ts
+++ redux-section-16/redux-demo/src/app/tasking/todo-list/todo-list.component.ts
@@ -3,27 +3,51 @@ import { NgRedux, select } from 'ng2-redux';
 
 import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO } from '../actions'; 
 import { IAppState } from '../../store'; 
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
   @select(s => s.tasking.todos) todos; 
+
+  @select((s:IAppState) => s.tasking.loadingList ) isLoading;
   
-  constructor(private ngRedux: NgRedux<IAppState>) {
+  @select((s:IAppState) => s.tasking.processing) processing;
+  
+  constructor(private ngRedux: NgRedux<IAppState>, private todoService:TodoService) {
+  }
+  ngOnInit(): void {
+    this.todoService.loadTodos();
+
+    // this.ngRedux.dispatch({type: 'FETCH_TODOS_REQUEST'})
+
+    // this.todoService
+    //   .getTodos()
+    //   .subscribe(
+    //     (response) => {
+    //       // // Non-redux way
+    //       // this.todos = response.json();
+          
+    //       //Redux way
+    //       this.ngRedux.dispatch({ type: 'FETCH_TODOS_SUCESS', todos: response.json() })
+    //     },
+    //     error => {
+    //       this.ngRedux.dispatch({type: ''})
+    //     }
+    //   )
   }
 
   addTodo(input) {
     if (!input.value) return; 
-
-    this.ngRedux.dispatch({ type: ADD_TODO, title: input.value });
-
-    input.value = '';
+    this.todoService.addTodo(input.value);
+     input.value = '';
   }
 
   toggleTodo(todo) {
+    console.log('modifing', todo);
     this.ngRedux.dispatch({ type: TOGGLE_TODO, id: todo.id });
   }
 
